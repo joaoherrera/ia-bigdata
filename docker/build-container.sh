80fe7bd9782f29dc7c32527e3c9e9fb1a2c86759
João Herrera <joao.herrera@outlook.com>
2023-04-01 22:11:50 -0300

    clean repository to keep the source code of the final project

---
+++ docker/build-container.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export UID=$(id -u)
+export GID=$(id -g)
+
+docker build --build-arg USER=$USER --build-arg PW=cv123 --build-arg UID=$UID --build-arg GID=$GID -t ia-bigdata .
+docker run -it --gpus all -v $(dirname `pwd`):/home/$USER/workspace/cv-hardware-parts --name cv-hardware-parts ia-bigdata
