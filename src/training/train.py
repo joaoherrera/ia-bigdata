383d43aa99eb411f5179496d81d269b43e3f8f34
João Herrera <joao.herrera@outlook.com>
2023-10-28 22:38:30 -0300

    Create argument parser in the training script

--- src/training/train.py
+++ src/training/train.py
@@ -1,16 +1,9 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Date: 2023-04-01                                                                                          #
-# Author: Joao Herrera                                                                                      #
-#                                                                                                           #
-# Script to train a classifier model of screws.                                                             #
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-
 import os
 from datetime import datetime
 
 import torch
 from torch.utils.data import DataLoader, random_split
+from argparse import ArgumentParser
 
 from src.dataset.augmentations import ScrewAugmentations
 from src.dataset.dataset import CocoDataset, DatasetUtils
@@ -20,40 +13,108 @@ from src.engine.trainer import SupervisedTrainer
 from src.training.tensorboard import TrainingRecorder
 
 
-def main():
-    dataset_root = "/home/joaoherrera/server/datasets/lemons"
-    dataset_images_directory = f"{dataset_root}/images"
-    dataset_annotations_file = f"{dataset_root}/annotations/annotations.json"
-    dataset_training_annotation_file = f"{dataset_root}/annotations/training_annotations.json"
-    dataset_validation_annotation_file = f"{dataset_root}/annotations/validation_annotations.json"
-    recorder = TrainingRecorder(f"{dataset_root}/experiments/training_{datetime.now().__str__()}")
+def main(args: dict) -> None:
+    recorder = TrainingRecorder(f"{args.get('output_path')}/training_{datetime.now().__str__()}")
     checkpoint_path = os.path.join(recorder.summary_filepath, "checkpoint.pth")
+
+    # Setup augmentations and preprocessing functions to be used during training.
+    augmentations_funcs = OrderedCompose([ScrewAugmentations.augment])
+    preprocessing_funcs = OrderedCompose([CocoPreprocessing.resize], size=(224, 224))
+
+    # ~~ Train subset
+    train_subset = CocoDataset(
+        args.get("training_images"),
+        args.get("training_annotations"),
+        augmentations=augmentations_funcs,
+        preprocessing=preprocessing_funcs,
+    )
+    train_dataloader = DataLoader(
+        train_subset,
+        batch_size=args.get("batch_size"),
+        shuffle=True,
+    )
+
+    # ~~ Validation subset
+    validation_subset = CocoDataset(
+        args.get("validation_images"),
+        args.get("validation_annotations"),
+        augmentations=None,
+        preprocessing=preprocessing_funcs,
+    )
+    validation_dataloader = DataLoader(
+        validation_subset,
+        batch_size=args.get("batch_size"),
+        shuffle=True,
+    )
+
+    # Training settings
+    loss_validation = torch.nn.BCELoss()
+    loss_train = torch.nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    epochs = args.get("epochs")
 
     model = ResNetClassifier(checkpoint_path)
     model.load()
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    loss_train = torch.nn.BCELoss()
-    loss_test = torch.nn.BCELoss()
-
-    augmentations_funcs = OrderedCompose([ScrewAugmentations.augment])
-    preprocessing_funcs = OrderedCompose([CocoPreprocessing.resize], size=(224, 224))
-    
-    train_subset = CocoDataset(dataset_images_directory, dataset_training_annotation_file)
-    test_subset = CocoDataset(dataset_images_directory, dataset_validation_annotation_file)
-    
-    test_subset.augmentations = None
-    
-    # Save annotations for both datasets
-    train_subset.tree.save(output_path=os.path.join(recorder.summary_filepath, "training_annotations.json"))
-    test_subset.tree.save(output_path=os.path.join(recorder.summary_filepath, "validation_annotations.json"))
- 
-    train_subset = DataLoader(train_subset, 16, shuffle=True)
-    test_subset = DataLoader(test_subset, 16, shuffle=True)
-    
     trainer = SupervisedTrainer(torch.device("cuda:0"), model, recorder)
-    trainer.fit(train_subset, test_subset, optimizer, loss_train, loss_test, 50, verbose=False)
+    trainer.fit(train_subset, validation_subset, optimizer, loss_train, loss_validation, epochs, verbose=False)
+
+
+def build_arg_parser() -> ArgumentParser:
+    parser = ArgumentParser(description="Train a deep learning model")
+
+    parser.add_argument(
+        "--training-images",
+        type=str,
+        help="Path to the directory containing training images.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--training-annotations",
+        type=str,
+        help="Path to the annotation file of the training set.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--validation-images",
+        type=str,
+        help="Path to the directory containing validation images.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--validation-annotations",
+        type=str,
+        help="Path to the annotation file of the validation set.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        help="Path to the output directory, where the model will be saved.",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        help="Batch size",
+        default=16,
+    )
+
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        help="Number of epochs",
+        default=50,
+    )
+
+    return parser
 
 
 if __name__ == "__main__":
-    main()
+    args = build_arg_parser().parse_args()
+    main(vars(args))
