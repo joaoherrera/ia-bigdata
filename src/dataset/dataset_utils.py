f4810cff05127dd3b56d330df4da0157a7e58629
João Herrera <joao.herrera@outlook.com>
2023-12-03 14:18:58 -0300

    First version of patch extractor

--- src/dataset/dataset_utils.py
+++ src/dataset/dataset_utils.py
@@ -3,10 +3,11 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 import os
-from typing import List
+from typing import List, Tuple
 
 import cv2
 import numpy as np
+from numpy.lib.stride_tricks import as_strided
 
 
 def read_image(image_path: str, read_mode: int = cv2.IMREAD_COLOR, channel_first: bool = False) -> np.ndarray:
@@ -49,3 +50,32 @@ def read_paths(directory_path: str) -> List[str]:
         raise ValueError(f"Path {directory_path} is not a directory.")
 
     return os.listdir(directory_path)
+
+
+def to_patches(image: np.ndarray, patch_size: int, stride: int) -> Tuple[List[np.ndarray], List[str]]:
+    # Shape of the output array of patches.
+    # o o x x x      x o o x x      x x o o x     x x x o o      x x x x x      x x x x x      x x x x x      x x x x x
+    # o o x x x  ->  x o o x x  ->  x x o o x  -> x x x o o  ->  o o x x x  ->  x o o x x  ->  x x o o x  ->  x x x o o
+    # x x x x x      x x x x x      x x x x x     x x x x x      o o x x x      x o o x x      x x o o x      x x x o o
+
+    # In this case, we have 8 matrices of 2 rows by 2 columns as result.
+    # And the resulting shape should be (2 (image rows), 4 (image columns), 2 (patch rows), 2 (patch columns))
+
+    rows, cols = image.shape[:2]
+    patch_rows = (rows - patch_size) // stride + 1
+    patch_cols = (cols - patch_size) // stride + 1
+    shape = (patch_rows * patch_cols + patch_rows, patch_size, patch_size)
+
+    # ndarray.strides gives us the number of bytes to move in the image to get to the next row or column.
+    bytes_patch_cols = image.strides[1]
+    bytes_patch_rows = image.strides[0]
+    bytes_shift = bytes_patch_cols * stride
+
+    patches = as_strided(x=image, shape=shape, strides=(bytes_shift, bytes_patch_rows, bytes_patch_cols))
+
+    # Remove patches that are not fully in the image, e.g.
+    # x x x x o
+    # o x x x o
+    # o x x x x
+
+    patches = np.delete(patches, obj=np.arange(patch_cols, patches.shape[0], patch_cols), axis=0)
