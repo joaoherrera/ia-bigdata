429d8a8db614442a1655b776297ffb2cc1a69f03
Bruno Drugowick Muniz <bruno.drugowick@gmail.com>
2023-12-17 10:56:57 -0300

    Add function to join patches (#11)
    * Add function to join patches
    
    * Remove additional blank line
    
    * Small adjustment on usage of resulting_shape
    
    * Reuse split instead of doing it again
    
    * Better names for variables
    
    * Initial PR review
    
    * Add filename_from_path function
    
    * Use bitwise operator
    
    ---------
    
    Co-authored-by: Bruno Drugowick <brunodrugowick@users.noreply.github.com>

--- src/dataset/dataset_utils.py
+++ src/dataset/dataset_utils.py
@@ -3,7 +3,7 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 import os
-from typing import List
+from typing import List, Tuple
 
 import cv2
 import numpy as np
@@ -51,3 +51,82 @@ def read_paths(directory_path: str) -> List[str]:
         raise ValueError(f"Path {directory_path} is not a directory.")
 
     return os.listdir(directory_path)
+
+
+@staticmethod
+def join_patches(patches: List[np.ndarray], filenames: List[str]) -> np.ndarray:
+    """Joins a list of patches into a single image.
+
+    Args:
+        patches (List[np.ndarray]): The patches to join.
+        filenames (List[str]): The filenames for the patches in the format <name>_x_y.<ext> 
+        where x and y represent a position of the patch in the original image.
+
+    Returns:
+        np.ndarray: A single image with all the patches merged together. When pixels collide
+        in two or more patches, the greather value is used.
+
+    Raises:
+        ValueError: If no patches are provided as input.
+    """
+    if len(patches) == 0:
+        raise ValueError("Input list of patches is empty.")
+    
+    patches = [patch.astype(np.uint8) for patch in patches]
+    patches_positions = [x_y_from_filename(filename_from_path(filename)) for filename in filenames]
+    result_shape = calculate_result_shape_from_patches(patches, patches_positions)
+    result_image = np.zeros(result_shape, dtype=np.uint8)
+
+    for (init_y, init_x), patch in zip(patches_positions, patches):
+        row, col = patch.shape
+        result_image[init_y:init_y + row, init_x:init_x + col] |= patch
+
+    return result_image
+
+
+def calculate_result_shape_from_patches(patches: List[np.ndarray], dimensions: Tuple[int, int]) -> Tuple[int, int]:
+    """Given a set of patches, calculates the resulting size of an image that contains all patches.
+
+    Args:
+        patches (List[np.ndarray]): The patches involved in the operation.
+        dimensions (Tuple[int, int]): A list of coordinates (x, y) of where, in the original images,
+        each patch is positioned.
+
+    Returns:
+        Tuple[int, int]: The size of each dimension of the resulting calculated image.
+    """
+    coords = list(zip(*dimensions))
+    shapes = list(zip(*[patch.shape[:2] for patch in patches]))
+    image_shape = np.max(np.array(shapes) + np.array(coords), axis=1)
+
+    return image_shape
+
+
+def x_y_from_filename(filename: str) -> Tuple[int, int]:
+    """Extracts x and y coordinates from a filename in the format <name>_x_y.<ext>.
+
+    Args:
+        filename (str): The name of the file in the expected format.
+
+    Returns:
+        Tuple[int, int]: The x and y coordinates extracted from the filename.
+    """
+    split_by_underscore = filename.split("_")
+    x = int(split_by_underscore[1])
+    y = int(split_by_underscore[2].split(".")[0])
+
+    return (x, y)
+
+def filename_from_path(path: str) -> str:
+    """Extracts filename from a path. Robus enough to receive a filename and
+    return if if not a path.
+    
+    Args:
+        path (str): A path to a file.
+        
+    Returns:
+        str: The extracted filename.
+    """
+    basename = os.path.basename(path)
+    filename, _ = os.path.splitext(basename)
+    return filename
